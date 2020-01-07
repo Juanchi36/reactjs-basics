1802dff49621d60ae713eb9ad857b378672c9b61
Juanchi36 <juanmartingon@hotmail.com>
2020-01-07 18:13:09 -0300

    Adding redux

--- src/app/index.js
+++ src/app/index.js
@@ -1,67 +1,111 @@
-import React from 'react';
-import { render } from 'react-dom';
+// import React from 'react';
+// import {render} from 'react-dom';
 
-import { Header } from "./components/Header";
-import { Home } from "./components/Home";
+// import { Main } from './components/Main'
+// import { User } from './components/User'
 
-class App extends React.Component {
-    constructor() {
-        super();
-        this.state = {
-            homeLink: 'Home',
-            homeMounted: true
-        }
-    }
-    onGreet() {
-        alert('Hello');
-    }
+// class App extends React.Component{
+//     constructor(){
+//         super();
+//         this.state = {
+//             username : "Max"
+//         }
+//     }
 
-    onChangeLinkName(newName) {
-        this.setState({
-            homeLink: newName
-        })
-    }
+//     onChageUsername(newName) {
+//         this.setState({
+//             username : newName
+//         })
+//     }
+//     render(){
+//         return(
+//             <div className="container">
+//                 <Main changeUsername={this.onChageUsername.bind(this)}/>
+//                 <hr/>
+//                 <User username={this.state.username}/>
+//             </div>
+//         );
+//     }
+// }
 
-    onChangeHmeMounted() {
-        this.setState({
-            homeMounted: !this.state.homeMounted
-        })
-    }
-    render() {
-        let homeCmp = '';
-        if (this.state.homeMounted){
-            homeCmp = (
-                <Home
-                    name={"Max"}
-                    initialAge={27}
-                    greet={this.onGreet}
-                    changeLink={this.onChangeLinkName.bind(this)}
-                initialLinkName={this.state.homeLink}
-                />
-            )
-        }
-        return (
-            <div className="container">
-                <div className="row">
-                    <div className="col-xs-10 col-xs-offset-1">
-                        <Header homeLink={this.state.homeLink} />
-                    </div>
-                </div>
-                <div className="row">
-                    <div className="col-xs-10 col-xs-offset-1">
-                        <Home
-                            name={"Max"}
-                            initialAge={27}
-                            greet={this.onGreet}
-                            changeLink={this.onChangeLinkName.bind(this)}
-                            initialLinkName={this.state.homeLink}
-                        />
+// render(<App />, window.document.getElementById('app'));
 
-                    </div>
-                </div>
-            </div>
-        );
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { createLogger } from 'redux-logger';
+
+
+const mathReducer = (state = {
+    result: 1,
+    lastValues: []
+}, action) => {
+    switch (action.type) {
+        case "ADD":
+            // state.result += action.payload;
+            state = {
+                ...state,
+                result: state.result + action.payload,
+                lastValues: [...state.lastValues, action.payload]
+            };
+            // state.lastValues.push(action.payload);
+            break;
+        case "SUBSTRACT":
+            // state.result -= action.payload;
+            state = {
+                ...state,
+                result: state.result - action.payload,
+                lastValues: [...state.lastValues, action.payload]
+            };
+            // state.lastValues.push(action.payload);
+            break;
     }
+    return state;
 }
 
-render(<App />, window.document.getElementById('app'));
+const userReducer = (state = {
+    name: 'Max',
+    age: 27
+}, action) => {
+    switch (action.type) {
+        case "SET_NAMR":
+            state = {
+                ...state,
+                name: action.payload
+            };
+            break;
+        case "SET_AGE":
+            state = {
+                ...state,
+                age: action.payload
+            };
+            break;
+    }
+    return state;
+}
+
+const myLogger = (store) => (next) =>(action) => {
+    console.log('Logged Action ', action);
+    next(action);
+}
+
+const store = createStore(combineReducers({mathReducer, userReducer}), {}, applyMiddleware(createLogger()));
+
+store.subscribe(() => {
+    // console.log('Store updated!', store.getState());
+});
+
+store.dispatch({
+    type: "ADD",
+    payload: 100
+});
+store.dispatch({
+    type: "ADD",
+    payload: 7
+});
+store.dispatch({
+    type: "SUBSTRACT",
+    payload: 44
+});
+store.dispatch({
+    type: "SET_AGE",
+    payload: 10
+});
