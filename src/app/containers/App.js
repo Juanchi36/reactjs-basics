ce9b7af475eec15727e3b6133ce7f8d6312b8567
Juanchi36 <juanmartingon@hotmail.com>
2020-01-13 16:01:02 -0300

    Agregando redux

---
+++ src/app/containers/App.js
@@ -0,0 +1,36 @@
+import React from 'react';
+
+import { Main } from '../components/Main';
+import { User } from '../components/User';
+import { connect } from 'react-redux';
+import { setName } from '../actions/userAction';
+
+class App extends React.Component {
+
+    render() {
+        return (
+            <div className="container">
+                <Main changeUsername={() => this.props.setName("Anna")} />
+                <hr />
+                <User username={this.props.user.name} />
+            </div>
+        );
+    }
+}
+
+const mapStateToProps = (state) => {
+    return {
+        user: state.user,
+        math: state.math
+    };
+};
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        setName: (name) => {
+            dispatch(setName(name));
+        }
+    };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
